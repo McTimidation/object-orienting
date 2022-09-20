3dad9bb2af8ca029b49e314634d2153ecc130f3a
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-09-20 16:19:35 +0000

    watercooler object

---
+++ main.js
@@ -0,0 +1,10 @@
+let waterCooler = {
+    power: Boolean,
+    tankLevel: 1,
+    temperature: ['hot','cold','room'],
+    pour: () => {
+        pourWater(temperature);
+    },
+    flavor: ['raspberry','tonic','cucumber','hard','seltzer']
+}
+
